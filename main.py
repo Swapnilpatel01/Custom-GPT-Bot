c806a5ca9dc7dc6f23cdfde31dbcd9581190d722
Swapnilpatel01 <42020767+Swapnilpatel01@users.noreply.github.com>
2023-08-04 00:54:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -48,10 +48,8 @@ index = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index_name
 def get_similar_docs(query, k=2, score=False):
     if score:
         similar_docs = index.similarity_search_with_score(query, k=k)
-        time.sleep(60)
     else:
         similar_docs = index.similarity_search(query, k=k)
-
     return similar_docs
 
 model_name = "gpt-3.5-turbo"
